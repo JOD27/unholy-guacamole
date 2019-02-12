de1528e2bbb16bb3fb81f1ff6a381b235eb8ba64
Brian Neville <briannev2010@gmail.com>
2019-02-12 16:11:09 +0000

    added break

--- app/src/main/java/com/example/unholy_guacamole/MyInputMethodService.java
+++ app/src/main/java/com/example/unholy_guacamole/MyInputMethodService.java
@@ -39,18 +39,15 @@ public class MyInputMethodService extends InputMethodService implements Keyboard
         InputConnection inputConnection = getCurrentInputConnection();
         if (inputConnection != null) {
             switch(primaryCode) {
-
-                case 32:
-                    inputConnection.commitText(" ", 1);
-                    break;
                 case Keyboard.KEYCODE_DELETE :
-                    CharSequence selectedText = inputConnection.getSelectedText(0);
+                CharSequence selectedText = inputConnection.getSelectedText(0);
 
-                    if (TextUtils.isEmpty(selectedText)) {
-                        inputConnection.deleteSurroundingText(2, 0);
-                    } else {
-                        inputConnection.commitText("", 1);
-                    }
+                if (TextUtils.isEmpty(selectedText)) {
+                    inputConnection.deleteSurroundingText(1, 0);
+                } else {
+                    inputConnection.commitText("", 1);
+                }
+                    break;
                 case Keyboard.KEYCODE_SHIFT:
                     caps = !caps;
                     keyboard.setShifted(caps);
@@ -60,14 +57,11 @@ public class MyInputMethodService extends InputMethodService implements Keyboard
                     inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
 
                     break;
-
-                default:
-
+                default :
                     char code = (char) primaryCode;
                     if(Character.isLetter(code) && caps){
                         code = Character.toUpperCase(code);
                     }
-
                     inputConnection.commitText(String.valueOf(code), 1);
 
 
